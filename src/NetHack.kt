dd696b68beed4b6c4345a372df70fd1967095686
Vanchinathan <vayipalayamchand@paypal.com>
2018-09-01 20:22:11 -0500

    update NetHack

--- src/NetHack.kt
+++ src/NetHack.kt
@@ -1,15 +1,15 @@
 fun main(args: Array<String>) {
-    var playerName = "Bingo"
+    val playerName = "Bingo"
     var health = 100
     var immortal = false
 
     if(health >= 100){
-        println(playerName + " is in good condition.")
+        println("$playerName is in good condition.")
     }else if (health > 70){
-        println(playerName + " is in OK condition.")
+        println("$playerName is in OK condition.")
     }
     else{
-        println(playerName + " is in awful condition.")
+        println("$playerName is in awful condition.")
     }
 
 
@@ -20,5 +20,31 @@ fun main(args: Array<String>) {
             else if (health > 70) "is in OK condition."
             else "is in awful condition."
 
-    println(playerName + " " + healthStatus)
+    // String templates
+    println("$playerName $healthStatus")
+
+    // String templates can have expressions
+    // ${if(someThing) "YES" else "NO"}
+
+
+    // using ranges in if expressions avoids ordering of the if-else statements
+    // until is a function to create a range and excludes upper bound
+    // downTo is another method, but, creates the range in descending order
+    val newHealthStatus = if(health == 100) "is in good condition."
+        else if (health in 0.until(70)) " is in awful condition!!"
+        else if (health in 70..99) " is in OK condition."
+        else "something terrible happened!!"
+
+    println("$playerName $newHealthStatus")
+
+    // if expressions can be replaced by when expressions , if there is a else if block in your code
+    val latestHealthStatus = when(health) {
+        100 -> "is in good condition."
+        in 0.until(70) -> "is in awful condition."
+        in 70.until(100) -> "is in OK condition."
+        else -> "Something went wrong"
+    }
+
+    println("$playerName $latestHealthStatus")
+
 }
